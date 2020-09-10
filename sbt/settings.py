af5338eb08178544130dbcf5fc91002db7c97d96
ronnil <ronniloreo@gmail.com>
2020-09-11 01:10:40 +0530

    added debug settings

--- sbt/settings.py
+++ sbt/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'io)y8-71d+8-kn7%*k&dk=4ri$=2&8i=!rvczc7c6m00o7tdno'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['164.90.213.239']
 
