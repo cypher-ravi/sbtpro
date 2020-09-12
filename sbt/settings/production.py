3aba9e06494d35ec3eef65cfa2384c7cf33d0a19
ronnil <ronniloreo@gmail.com>
2020-09-12 15:20:22 +0530

    save default settings to production

--- sbt/settings/production.py
+++ sbt/settings/production.py
@@ -5,6 +5,11 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['164.90.213.239','stacksoftwares.in','www.stacksoftwares.in','127.0.0.1']
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'io)y8-71d+8-kn7%*k&dk=4ri$=2&8i=!rvczc7c6m00o7tdno'
+
+
+
 
 # Application definition
 
