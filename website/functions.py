edf08ee08636f90b4154581637a3482888041dcf
Akarsh711 <paliwalap7@gmail.com>
2020-10-04 19:18:49 +0530

    added validations for form s and change it's code to sperate file known as validations.py

--- website/functions.py
+++ website/functions.py
@@ -6,34 +6,3 @@ def send_sms_message(number):
     message = f"Thank you for your great interest \nDownload Our App For\n Discount and Exciting offers {app_link_for_playstore}"
     
     return number
-
-def discount_validation(plan_id, discount, amount):
-    # to do make secure multiplier in purchase view
-    # change the dict passing for plan_review
-    try:
-        print('try block')
-        id = plan_id
-        print(id)
-        plan = Plan.objects.get(plan_id = id)
-    except:  
-        print('exception occured...')
-        return ({'discount_applied':'' ,'total':'' ,'error': 'entered amount is invalid'})
-    if plan.plan_amount == amount:
-        if  discount == 0:
-            return ({'discount_applied':'' ,'total':'' ,'error': 'Value should not be 0'})
-        print('plan')
-        print(discount)
-        if discount >= plan.minimum_discount and discount <= plan.maximum_discount:
-            print('min max')
-            amount = plan.plan_amount
-            # default_val = 100
-            
-            total = discount * amount
-            return ({'discount_applied': discount,'total':total ,'error': None})
-        else:
-            print('else min max')
-            return ({'discount_applied':'' ,'total':'' ,'error': 'entered amount is invalid'})
-    else:
-        print('critical error')
-        return ({'discount_applied':'' ,'total':'' ,'error': 'Plan Amount is Invalid'})
-    return ({'error':'Invalid Request'})
