e705a946b4804ff636450cb3042dd4b8f646fafe
ronnil <ronniloreo@gmail.com>
2020-10-16 13:24:59 +0530

    register models to admin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,10 +1,8 @@
-# from django.contrib import admin
-# from .models import *
+from django.contrib import admin
+from .models import *
 # # Register your models here.
-# admin.site.register(Branch)
-# # admin.site.register(Employee)
-# admin.site.register(Banner)
-# admin.site.register(Banner2)
-# # admin.site.register(Customer)
-# # admin.site.register(DailyAttendance)
+admin.site.register(Branch)
+admin.site.register(Banner)
+admin.site.register(Banner2)
+
 
