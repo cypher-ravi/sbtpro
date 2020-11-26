1166910299b13e6996929f722323ca9664f2817d
cypher-ravi <ronniloreo@gmail.com>
2020-11-26 17:21:00 +0530

    update filters in customer and employee

--- Customer/admin.py
+++ Customer/admin.py
@@ -6,8 +6,8 @@ from .models import Customer
 
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ('customer_id', 'customer_name','customer_is_active','subscription_plan_taken','user')
-    search_fields = ['customer_id', 'customer_name']
+    search_fields = ['customer_id', 'customer_name','user__phone']
     list_per_page = 50
-    list_filter = ['customer_is_active','subscription_plan_taken']
+    list_filter = ['customer_is_active','subscription_plan_taken','joining_date']
 
 admin.site.register(Customer,CustomerAdmin)
